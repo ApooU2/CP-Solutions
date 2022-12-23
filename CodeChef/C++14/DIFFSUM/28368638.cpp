a53b558d94221b4036927a17bab4091cf0d1745b
Apoorva Upadhyaya <apoorvaupadhyaya2@gmail.com>
2022-12-22 19:09:32 -0600

    Committing files

---
+++ CodeChef/C++14/DIFFSUM/28368638.cpp
@@ -0,0 +1,22 @@
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a , b;
+  cin>>a>>b;
+  if(a>b)
+  {
+      cout<<a-b<<endl;
+  }
+  else
+  {
+      cout<<a+b<<endl;
+  }
+   
+	return 0;
+}
